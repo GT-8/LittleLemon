55e177c42df1ef19228a0862322a6b611b843580
GT-8 <diarmando2@gmail.com>
2024-08-15 11:51:53 -0600

    tests 2

--- littlelemon_DACP/tests/test_views.py
+++ littlelemon_DACP/tests/test_views.py
@@ -1,22 +1,31 @@
 from django.test import TestCase, Client
-from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APIClient
 from restaurant.models import Menu
-from restaurant.serializers import MenuSerializer
-from django.contrib.auth.models import User
+from LittleLemonAPI.serializers import MenuSerializer
 
-class MenuViewTest(TestCase):
+class MenuTest(TestCase):
 
     def setUp(self):
-        self.menu1 = Menu.objects.create(id=11,title="IceCream", price=80, inventory=101)
-        self.menu2 = Menu.objects.create(id=12,title="IceCream", price=80, inventory=102)
-    
-   
+        Menu.objects.create(name="TestCream", price=1.8, menu_item_description="test 1", dish="Main Test")
+        Menu.objects.create(name="Testlate", price=2.4, menu_item_description="test 2", dish="Testlad")
+
     def test_getall(self):
-        user = User.objects.create_user(username='testuser', password='testpassword')
-        client = Client()
-        client.force_login(user)
-        response = client.get("http://127.0.0.1:8000/restaurant/menu/")
-        serialized_menus = 200
-        self.assertEqual(response.status_code, serialized_menus)
+        client = APIClient()
+
+        for i in Menu.objects.all():
+            response = client.get("http://127.0.0.1:8000/api/menu-items/" + str(i.id))
+            self.assertEqual(response.data, MenuSerializer(i).data)
+                
+
+class MenuItemTest(TestCase):
+
+    def setUp(self):
+        self.item = Menu.objects.create(name="TestBerry", price=0.8, menu_item_description="test i")
+
+    def test_getitem(self):
+        client = APIClient()
+
+        serialized = MenuSerializer(self.item)
+        response = client.get("http://127.0.0.1:8000/api/menu-items/" + str(self.item.id))
+
+        self.assertEqual(response.data, serialized.data)
